5bb54851891a99445bf353c0040fd399e16b931d
Joao <joao.schilian.ferreira@escola.pr.gov.br>
2024-03-27 10:42:03 -0300

    Subindo exercícios python

---
+++ aula01.py
@@ -0,0 +1,12 @@
+import os
+
+#print("Olá mundo cruel")
+print("O 3DS é a turma número 1 do Sagrada")
+turma = "2DS"
+pos = 2
+
+print("O", turma, "é a turma número", pos, "do Sagrada")
+print(type(turma))
+print(type(pos))
+
+os.system("clear")
